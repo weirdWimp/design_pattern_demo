371cd65014c5efb7e4d86cc976b1f68aefa52151
SplendidGuo <splendidmonkey@hotmail.com>
2020-04-18 23:07:21 +0800

    add the comment result

--- src/main/java/learn/something/prototype/PrototypeMain.java
+++ src/main/java/learn/something/prototype/PrototypeMain.java
@@ -16,18 +16,61 @@ public class PrototypeMain {
         String commentLine = "On November 14th of 2017, Firefox 57 was released, and JSONView stopped working. This was not unexpected - Mozilla had been warning for a while that they were going to shut off their old extension system completely and move to the WebExtension system pioneered by Google Chrome. All of the Firefox extensions using the old system were silently disabled. I had hoped to have a new version of JSONView available that was compatible with this new system before Firefox 57 released, but unfortunately the deprecation coincided with a crunch period...";
         pound.comment(commentLine);
 
-        System.out.println("\n\n");
+        // ##################################################################################
+        // #  On November 14th of 2017, Firefox 57 was released, and                        #
+        // #  JSONView stopped working. This was not unexpected - Mozilla had               #
+        // #  been warning for a while that they were going to                              #
+        // #  shut off their old extension system completely and move to                    #
+        // #  the WebExtension system pioneered by Google Chrome. All of the                #
+        // #  Firefox extensions using the old system were silently disabled. I             #
+        // #  had hoped to have a new version of JSONView available                         #
+        // #  that was compatible with this new system before Firefox 57                    #
+        // #  released, but unfortunately the deprecation coincided with a crunch period... #
+        // ##################################################################################
 
+
+        System.out.println("\n\n");
         Comment startPound = manager.create("start_pound");
         startPound.comment(commentLine);
+
+        // #  On November 14th of 2017, Firefox 57 was released, and
+        // #  JSONView stopped working. This was not unexpected - Mozilla had
+        // #  been warning for a while that they were going to
+        // #  shut off their old extension system completely and move to
+        // #  the WebExtension system pioneered by Google Chrome. All of the
+        // #  Firefox extensions using the old system were silently disabled. I
+        // #  had hoped to have a new version of JSONView available
+        // #  that was compatible with this new system before Firefox 57
+        // #  released, but unfortunately the deprecation coincided with a crunch period...
 
         System.out.println("\n\n");
         Comment slash = manager.create("slash");
         slash.comment(commentLine);
 
+        //  On November 14th of 2017, Firefox 57 was released, and
+        //  JSONView stopped working. This was not unexpected - Mozilla had
+        //  been warning for a while that they were going to
+        //  shut off their old extension system completely and move to
+        //  the WebExtension system pioneered by Google Chrome. All of the
+        //  Firefox extensions using the old system were silently disabled. I
+        //  had hoped to have a new version of JSONView available
+        //  that was compatible with this new system before Firefox 57
+        //  released, but unfortunately the deprecation coincided with a crunch period...
+
+
         System.out.println("\n\n");
         Comment dash = manager.create("dash");
         dash.comment(commentLine);
+
+        // -------------------------------------------------------------------------------------------------------------------------------------
+        // -  On November 14th of 2017, Firefox 57 was released, and JSONView stopped working. This was not unexpected - Mozilla had           -
+        // -  been warning for a while that they were going to shut off their old extension system completely and move to                      -
+        // -  the WebExtension system pioneered by Google Chrome. All of the Firefox extensions using the old system were silently disabled. I -
+        // -  had hoped to have a new version of JSONView available that was compatible with this new system before Firefox 57                 -
+        // -  released, but unfortunately the deprecation coincided with a crunch period...                                                    -
+        // -------------------------------------------------------------------------------------------------------------------------------------
+
+
     }
 
 
